0525aaa8df8c17924463dd815416cdbaae8c66e6
Vikash Singh Bhadoriya <codingwithvikash@gmail.com>
2021-07-02 16:33:24 +0530

    added Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+class Main{
+   public static void main(String args[]){  
+       
+        int fact=1;  
+        //Example to show factorial of 10
+        for(int i=2;i<=10;i++){   
+             fact*=i;    
+        }
+        System.out.println("Factorial of 10 is: "+fact);    
+   }  
+}
